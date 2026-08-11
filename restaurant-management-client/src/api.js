cf0ae54761364b44f3fe3ba3a6da6983cb9cdd35
sanvika05 <vannempalli.sanvika2021@vitstudent.ac.in>
2026-08-11 20:50:53 +0530

    Connect frontend to deployed API

--- restaurant-management-client/src/api.js
+++ restaurant-management-client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:5000/api";
+const baseURL = "https://restaurantmanagementsystem-qs1a.onrender.com/api";
 
 const getToken = () => localStorage.getItem("restaurant_token");
 
